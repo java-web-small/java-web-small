bf2e464b1cc168998350c2d463bde2945086417e
AnTNM <antnm@splus-software.com.vn>
2025-05-21 17:06:25 +0700

    feat: test notification discord when job fail

--- src/main/java/org/springframework/samples/petclinic/owner/VisitController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/VisitController.java
@@ -93,7 +93,7 @@ class VisitController {
 
 		owner.addVisit(petId, visit);
 		this.owners.save(owner);
-		redirectAttributes.addFlashAttribute1("message", "Your visit has been bookedddddd");
+		redirectAttributes.addFlashAttribute("message", "Your visit has been booked");
 		return "redirect:/owners/{ownerId}";
 	}
 
